775dcae11efcb860c5cb51206aff6d565ef6ad14
Gregory Maddra <bloxgate@gmail.com>
2021-10-13 22:20:33 -0500

    Don't include particle textures

--- blockhandlers/CubeColumnHandler.py
+++ blockhandlers/CubeColumnHandler.py
@@ -18,6 +18,8 @@ class CubeColumnHandler(BlockHandler):
     def handle(self, blockFile, blockModel, modID):
         entry = f"block:id=%{os.path.splitext(blockFile)[0]},data=*,"
         for key in blockModel['textures'].keys():
+            if key == 'particle':
+                continue
             lookupKey = self.determine_namespace_and_adjust_for_lookup(blockModel['textures'][key])
             txtSide = self.convert_texture_key_to_side(key)
             txt = BlockHandler.lookup_texture(lookupKey[0], modID, lookupKey[1])
